0dcc00cbbd75b468d20c16e2fe5ae5a6bf3b2ee1
EnergyPower233 <energy_power233@qq.com>
2026-09-21 15:54:35 +0800

    fix: include<cstdint>

--- src/common/io/io.h
+++ src/common/io/io.h
@@ -15,7 +15,7 @@ See the Mulan PSL v2 for more details. */
 #pragma once
 
 #include <vector>
-
+#include <cstdint>
 #include "common/defs.h"
 #include "common/lang/string.h"
 #include "common/lang/vector.h"
